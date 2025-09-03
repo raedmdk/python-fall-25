0ed792594e6744cfa7c6b1e256af1798418673d3
raedmdk <rkiswani@students.mchenry.edu>
2025-09-02 21:10:58 -0500

    push

---
+++ new_program. py 1.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+new_program.py
+--------------
+Assignment: Uploading to GitHub through Visual Studio Code
+
+This is a simple Python program created for the GitHub upload assignment.
+It demonstrates that a file can be added, committed, and pushed to a repository.
+
+Author: Raed Kiswani
+Date: September 2, 2025
+"""
+
+print("Hello, GitHub!")
